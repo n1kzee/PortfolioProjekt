55af271803ca6cc41bf4b648daf75afe3e9f5a24
Nick Zohren <nick.zohren@outlook.com>
2020-02-10 16:38:24 +0100

    InitProject

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  
+
+public class Character : MonoBehaviour
+{
+    //SerializeFields
+    [SerializeField]
+    private float m_Speed;
+   //Private Fields
+   //public FIelds
+   //Property Fields
+}
---
+++ Assets/Scripts/TESETRESOLUTION.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESETRESOLUTION : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 m_Resolution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForScreenChange(true, m_Resolution));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator WaitForScreenChange(bool fullscreen, Vector2 _resolution)
+    {
+        int width = (int)_resolution.x;
+        int height = (int)_resolution.y;
+
+        Screen.fullScreen = fullscreen;
+
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+
+        Screen.SetResolution(width, height, Screen.fullScreen);
+    }
+
+   
+}
